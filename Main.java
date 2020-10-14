77cf7702133591a7bcf037f542d740b8a54276d3
Chleb38 <57624882+Chleb38@users.noreply.github.com>
2020-10-14 17:43:41 +0200

    Revert "poprawione"
    This reverts commit 2129e135a3f41d5bb9dc750f7de51b8792fc4c16.

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         //powinno byc "Witaj swiecie!"
-        System.out.println("Witaj swiecie!");
+        System.out.println("Wtej swiecie!");
     }
 
 }
